34cc43a968fbcfadcc846f0dbfeb0bfded448b1f
Saurabh Shrivastava <saurabhshrivastava956@gmail.com>
2020-08-17 16:49:04 +0530

    Payement Method Working

--- lib/main.dart
+++ lib/main.dart
@@ -24,34 +24,24 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState() {
+    return MyHomePageState();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class MyHomePageState extends State<MyHomePage> {
   String cardNumber = '';
   String expiryDate = '';
   String cardHolderName = '';
   String cvvCode = '';
   bool isCvvFocused = false;
-  PaymentMethod _paymentMethod;
-  PaymentIntentResult _paymentIntent;
-
-  @override
-  void initState() {
-    super.initState();
-    StripePayment.setOptions(StripeOptions(
-        publishableKey:
-            "pk_test_51H6kE3FGnI1zKRtpqHdeOPN0jvTKDNgCIU3P9LnQGLNlp0UWJMDWov3sHPySxlBrECWAZB6nMcvJTdel1arlWmtF00OngFgvZ1",
-        merchantId: "",
-        androidPayMode: "test"));
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: SafeArea(
+        child: Column(
           children: <Widget>[
             CreditCardWidget(
               cardNumber: cardNumber,
@@ -62,33 +52,19 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Expanded(
               child: SingleChildScrollView(
-                child: CreditCardForm(
-                  onCreditCardModelChange: onCreditCardModelChange,
+                child: Column(
+                  children: [
+                    CreditCardForm(
+                      onCreditCardModelChange: onCreditCardModelChange,
+                    ),
+                    RaisedButton(
+                        child: Text('Add This Card'),
+                        color: Colors.blue,
+                        onPressed: () => print('working'))
+                  ],
                 ),
               ),
-            ),
-            RaisedButton(
-                child: Text("Add this card"),
-                color: Colors.blue,
-                onPressed: () {
-                  print("getting called");
-                }
-                // () async {
-                //   print("start");
-                //   _paymentMethod = await StripePayment.createPaymentMethod(
-                //     PaymentMethodRequest(
-                //       card: CreditCard(
-                //         name: cardHolderName,
-                //         number: cardNumber,
-                //         cvc: cvvCode,
-                //         expMonth: int.parse(expiryDate.split("/")[0]),
-                //         expYear: int.parse(expiryDate.split("/")[1]),
-                //       ),
-                //     ),
-                //   );
-                //   print("Success");
-                // },
-                )
+            )
           ],
         ),
       ),
@@ -105,3 +81,102 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 }
+
+// class MyHomePage extends StatefulWidget {
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   String cardNumber = '';
+//   String expiryDate = '';
+//   String cardHolderName = '';
+//   String cvvCode = '';
+//   bool isCvvFocused = false;
+//   PaymentMethod _paymentMethod;
+//   PaymentIntentResult _paymentIntent;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     StripePayment.setOptions(StripeOptions(
+//         publishableKey:
+//             "pk_test_51H6kE3FGnI1zKRtpqHdeOPN0jvTKDNgCIU3P9LnQGLNlp0UWJMDWov3sHPySxlBrECWAZB6nMcvJTdel1arlWmtF00OngFgvZ1",
+//         merchantId: "",
+//         androidPayMode: "test"));
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     print("Called");
+//     return Container(
+//       child: Scaffold(
+//         body: Column(
+//           crossAxisAlignment: CrossAxisAlignment.stretch,
+//           children: <Widget>[
+//             CreditCardWidget(
+//               cardNumber: cardNumber,
+//               expiryDate: expiryDate,
+//               cardHolderName: cardHolderName,
+//               cvvCode: cvvCode,
+//               showBackView: isCvvFocused,
+//             ),
+//             Expanded(
+//               child: SingleChildScrollView(
+//                 child: Column(
+//                   children: <Widget>[
+//                     CreditCardForm(
+//                       onCreditCardModelChange: onCreditCardModelChange,
+//                     ),
+//                     RaisedButton(
+//                       child: Text("Add this Card"),
+//                       color: Colors.blue,
+//                       onPressed: () {
+//                         print("Working");
+//                       },
+//                     )
+//                   ],
+//                 ),
+//               ),
+//             ),
+//             // RaisedButton(
+//             //   child: Text("Add this card"),
+//             //   color: Colors.blue,
+//             //   onPressed: () {
+//             //     print("CardNUmber :  abchdhd");
+//             //   },
+//             //   // onPressed: () {
+//             //   //   print("start");
+//             //   //   StripePayment.createPaymentMethod(
+//             //   //     PaymentMethodRequest(
+//             //   //       card: CreditCard(
+//             //   //         name: cardHolderName,
+//             //   //         number: cardNumber,
+//             //   //         cvc: cvvCode,
+//             //   //         expMonth: int.parse(expiryDate.split("/")[0]),
+//             //   //         expYear: int.parse(expiryDate.split("/")[1]),
+//             //   //       ),
+//             //   //     ),
+//             //   //   ).then((paymentMethod) {
+//             //   //     setState(() {
+//             //   //       _paymentMethod = paymentMethod;
+//             //   //     });
+//             //   //   });
+//             //   // },
+//             // )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+//   void onCreditCardModelChange(CreditCardModel creditCardModel) {
+//     setState(() {
+//       cardNumber = creditCardModel.cardNumber;
+//       expiryDate = creditCardModel.expiryDate;
+//       cardHolderName = creditCardModel.cardHolderName;
+//       cvvCode = creditCardModel.cvvCode;
+//       isCvvFocused = creditCardModel.isCvvFocused;
+//     });
+//   }
+// }
